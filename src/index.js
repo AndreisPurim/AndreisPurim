3b62438d70769ce4f1b59a9bc5f0de778c1a50d6
Andreis Purim <andreispurim@gmail.com>
2023-10-12 19:05:45 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { Document, Page } from 'react-pdf'
+import { PDFDocument } from 'pdfjs-dist';
+import { pdfjs } from 'react-pdf';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+function CustomPage({ obj }){
+  React.useEffect(()=>{
+    fetch("./files/"+obj.link,{ responseType: 'arraybuffer',}
+    ).then(function(res){
+      const reader = new FileReader();
+      console.log(res.data)
+    })
+      // const pdf = PDFDocument.load(res);
+    //   const pages = pdf.getPages();
+    //   let extractedText = '';
+
+    //   for (const page of pages) {
+    //     const textContent = page.getTextContent();
+    //     const pageText = textContent.items.map((item) => item.str).join(' ');
+    //     extractedText += pageText;
+    //   }
+    //   console.log(extractedText)
+    // })
+  })
+  return(
+    <>
+      hey
+      {obj.link}
+    </>
+  )
+}
+
+function App(){
+  const [links, setLinks] = React.useState(null);
+
+  React.useEffect(()=>{
+    fetch("./files.json",{}
+    ).then(function(res){
+      return res.json()
+    }).then(function(obj) {
+      const links_list = [
+        {
+          path: "/",
+          element: (
+            <ul>
+                {obj.files.map(link =>
+                  <li>
+                    <a href={"/"+link.name}>
+                      {link.name}
+                    </a>
+                  </li>
+                )
+                }
+            </ul>
+          ),
+        },
+        ...obj.files.map(function(elem) {
+          return {
+            path: "/"+elem.name,
+            element: <CustomPage obj={elem}/>
+          }
+        } 
+        )
+      ]
+      console.log(links_list)
+      setLinks(createBrowserRouter(links_list))
+    })
+  }, [])
+
+
+  return(
+    <>
+    {links? <RouterProvider router={links} />: null}
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
