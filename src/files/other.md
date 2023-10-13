01aaa05b140d194f31bddcf39192117170b91d5d
Andreis Purim <andreispurim@gmail.com>
2023-10-12 22:49:37 -0300

    Branching before hugo version

---
+++ src/files/other.md
@@ -0,0 +1,5 @@
+ASD
+ASDASD
+
+asd
+
--- src/index.js
+++ src/index.js
@@ -6,142 +6,48 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/TextLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import Markdown from 'react-markdown'
 
 
-// const CustomPage = () => {
-//   const [numPages, setNumPages] = React.useState();
-//   const [pageNumber, setPageNumber] = React.useState(1);
-//   const [text, setText] = React.useState('');
 
-//   React.useEffect(()=>{
-
-      
-//   }, [])
-
-//   const onDocumentLoadSuccess = async (e) => {
-//     setNumPages(e.numPages);
-//     //e.getPage(1)
-//     var a = await e.getPage(1)
-//     var b = await a.getTextContent()
-//     const pageText = b.items.map((item) => item.str).join(' ');
-//     console.log(pageText)
-//     setText(text)
-//   }
-
-//   return (
-//     <>
-//       <Document file={'sample.pdf'} onLoadSuccess={onDocumentLoadSuccess} />
-//       {text}
-//     </>
-//   );
-// };
-
-// // function CustomPage({ obj }){
-
-// //   const [numPages, setNumPages] = React.useState();
-// //   const [pageNumber, setPageNumber] = React.useState(1);
-
-// //   const onDocumentLoadSuccess = ({ numPages }) => {
-// //     setNumPages(numPages);
-// //     }
-
-// //   return (
-// //     <div>
-// //       asdasd
-// //       aaaa
-// //       <Document file="sample.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-// //         <Page pageNumber={pageNumber} />
-// //       </Document>
-// //       aaaaaaaaaaaaaaaaaaa
-// //       <p>
-// //         Page {pageNumber} of {numPages}
-// //       </p>
-// //     </div>
-// //   );
-// // }
-
-
-async function getContent(src){
-  const doc = await pdfjs.getDocument(src).promise
-  const page = await doc.getPage(1)
-  const text = await page.getTextContent()
-  return text
+function importAll(r) {
+  return r.keys().map(r);
 }
 
-async function getItems(src){
-  const content = await getContent(src)
-  let blob = new Blob([content], {type: 'application/pdf'})
-  let content = window.URL.createObjectURL(blob)
-  const pageText = content.items.map((item) => item.str).join(' ');
-  // const items = content.items.map((item) =>
-  //   {
-  //     console.log(item.str)
-  //   }
-  // )
-  return pageText
-}
+const rawArticles =  importAll(require.context('./files', false, /\.md/)).map(file => 
+  fetch(file).then((response) => response.text()).then((text) => {
+    return {name: file, text: text}
+  }
+))
 
-function CustomPage(){
-  const [text, setText] = React.useState('')
-  React.useEffect(()=>{
-    async function fetchData() {
-      const t = await getItems('INCBAC.pdf')
-      setText(t)
-    }
-    fetchData();
-
-  }, [])
-  return(
-    <>
-      {text}
-    </>
-  )
-
-}
-
-const files_manager = require('./files/files.json')
 
 function App(){
-  const [links, setLinks] = React.useState(null);
-
+  const [articles, setArticles] = React.useState()
   React.useEffect(()=>{
-      const links_list = [
-        {
-          path: "/",
-          element: (
-            <ul>
-                {files_manager.map(link =>
-                  <li key={link.name}>
-                    <a href={"/"+link.name}>
-                      {link.name}
-                    </a>
-                  </li>
-                )
-                }
-            </ul>
-          ),
-        },
-        ...files_manager.map(function(elem) {
-          return {
-            path: "/"+elem.name,
-            element: <CustomPage obj={elem}/>
-          }
-        } 
-        )
-      ]
-      setLinks(createBrowserRouter(links_list))
+    async function fetchData() {
+      Promise.all(rawArticles).then((values) => {
+        setArticles(values)
+      });
+    }
+    fetchData()
   }, [])
 
 
-  return(
-    <>
-    {links? <RouterProvider router={links} />: null}
-    </>
-  )
+
+  if(articles){
+      return(
+      
+      // <Routes>
+
+
+      //</Routes>
+      <>
+      {articles?articles.map(art=> <div><Markdown>{art.text}</Markdown></div>):null}
+      </>
+    )}
+  else{
+    return(<h1>Loading</h1>)
+  }
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
